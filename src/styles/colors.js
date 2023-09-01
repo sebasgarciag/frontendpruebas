e6949e23e54bfefb22d600ba2fb0b40652bda8a3
sebasgarciag <sebastiangrciag@gmail.com>
2023-09-01 11:21:39 -0600

    prueba

---
+++ src/styles/colors.js
@@ -0,0 +1,12 @@
+const colors = {
+    rojo: '#F74A3E',
+    naranja_fuerte: '#F79420',
+    naranja_bajito: '#FBB03B',
+    blanco: '#FFFFFF',
+    morado_fuerte: '#484495',
+    morado_bajito: '#6A64AA',
+    color7: '#8F8B36',
+    color8: '#8F8B36', 
+};
+
+export default colors;
